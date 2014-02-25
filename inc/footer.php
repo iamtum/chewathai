b014c26ca73141bf40a557df5d80e433e7907f34
desperados <satsavira@hotmail.com>
2014-02-25 17:50:47 +0700

    up

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,7 @@
 <script src="js/jquery-1.8.3.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/selectivizr.js"></script>
+<script src="js/jquery.easydropdown.js"></script>
 <?php  //if(isset($js)) foreach ($js as $key => $value) echo'<script src="js/' . $value . '"></script>'; ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,31 +6,69 @@
 ?>
 <?php include("inc/header.php") ?>
 
+
 <div class="container">
+	<select class="dropdown" tabindex="9" data-settings='{"wrapperClass":"flat"}'>
+      <option value="1">TH</option>
+      <option value="2">EN</option>
+    </select>
+    <!--  style="display:none;"  border:1px red solid  -->
+    <nav class="mainmenu">
+    	<ul>
+            <li><a href="javascript:;">HOME</a></li>
+            <li><a href="javascript:;">ABOUT CHEWATHAI</a></li>
+            <li><a href="javascript:;">RESIDENCE</a></li>
+            <li><a href="index.php" style="padding:0px;"><img src="image/brand.gif" /></a></li>
+            <li><a href="javascript:;">INDUSTRIAL</a></li>
+            <li><a href="javascript:;">NEWS &amp; PROMOTION</a></li>
+            <li><a href="javascript:;">CONTACT US</a></li>
+        </ul>
+    </nav>
 	<div class="bigbanner">
     	<img src="image/bigbanner.jpg" /> 
     </div>
-    <div class="branch" style="background:#817f79;">
-        <div class="row">
-            <div class="col-md-3"><img src="image/brance/1.gif" /></div>
-            <div class="col-md-3"><img src="image/brance/2.gif" /></div>
-            <div class="col-md-3"><img src="image/brance/3.gif" /></div>
-            <div class="col-md-3"><img src="image/brance/4.gif" /></div>
-        </div>
+    <div class="branch" style="display:none;">
+        <ul>
+            <li><a href="javascript:;"><img src="image/brance/1.gif" /></a></li>
+            <li><a href="javascript:;"><img src="image/brance/2.gif" /></a></li>
+            <li><a href="javascript:;"><img src="image/brance/3.gif" /></a></li>
+            <li><a href="javascript:;"><img src="image/brance/4.gif" /></a></li>
+            <li><a href="javascript:;"><img src="image/brance/5.gif" /></a></li>
+        </ul>
     </div>
 
-  <div>News adn Promotion</div>
+  <header class="hnew"><h2>NEWS <span class="red">&amp;</span> PROMOTION</h2></header>
+  <div style="width:88%; margin:40px auto;">
     <div class="row">
     	<div class="col-md-6">
-        	<h2>News</h2>
-            braa
-      </div>
+        	<div>
+            	<div class="row">
+                	<div class="col-sm-6"><a href="javascript:;"><img src="image/img1.jpg" style="border:14px #fff solid; margin:0 auto;" /></a></div>
+                    <div class="col-sm-6">
+                        <h3>Sport Day and WingWOW Party Chewathai,TEI,Getco</h3>
+                        <p>กิจกรรมสันทนาการระหว่างบริษัทชีวาทัย และบริษัทในเครื่อ TEI,GETCO ร่วมสร้างสามัคคีภายในงานมีการจัดการแข่งขันกีฬา</p>
+                        <a href="javascript:;"><img src="image/btn/more.gif" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="col-md-6">
-        	<h2>Promotion</h2>
-            bra
-      </div>
+        	<div>
+            	<div class="row">
+                	<div class="col-sm-6"><a href="javascript:;"><img src="image/img2.jpg" style="border:14px #fff solid;" /></a></div>
+                    <div class="col-sm-6">
+                        <h3>Lorem ipsum dolor sit ame consectetur tincidunt</h3>
+                        <p>กิจกรรมสันทนาการระหว่างบริษัทชีวาทัย และบริษัทในเครื่อ TEI,GETCO ร่วมสร้างสามัคคีภายในงานมีการจัดการแข่งขันกีฬา</p>
+                        <a href="javascript:;"><img src="image/btn/more.gif" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="row">
+  </div>
+  
+     
+    <!--<div class="row">
     	<div class="col-md-6">
         	<h2>Progress</h2>
             <div style="background:#CCC; padding:20px;">
@@ -38,14 +76,6 @@
                 <p>Iure, quidem, quibusdam enim consequuntur optio temporibus corporis ipsam dolorum error officiis ducimus ab tempore at voluptatibus ipsum quos saepe inventore</p>
                 <div class="row">
                 	<div class="col-sm-6">
-                        <!--<div class="row">
-                            <div class="col-xs-6"><img src="image/data.jpg" width="200" height="200"></div>
-                            <div class="col-xs-6"><img src="image/data.jpg" width="200" height="200"></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-6"><img src="image/data.jpg" width="200" height="200"></div>
-                            <div class="col-xs-6"><img src="image/data.jpg" width="200" height="200"></div>
-                        </div>-->
                         <ul class="list_pic_prog clearfix">
                         	<li><img src="image/data.jpg" width="200" height="200"></li>
                             <li><img src="image/data.jpg" width="200" height="200"></li>
@@ -75,7 +105,13 @@
                 <div class="pull-right">list nav</div>
             </div>
         </div>
-    </div>
+    </div>-->
 </div>
-in body
+<!--<div style="width:1000px;">
+<div class="row">
+<div class="col-md-6" style="background:red; height:50px;">555555555555555</div>
+<div class="col-md-6" style="background:green; padding-left:120px;  height:50px;">555555555555555</div>
+</div>-->
+
+
 <?php include("inc/footer.php") ?>
